4b4dc1cc5a7437a46f7daadb9dedf7e7c13e3342
Parth Uppal <uppalparth523@gmail.com>
2025-10-31 23:12:32 +0530

    Update login.js

--- school-inventory-frontend/login.js
+++ school-inventory-frontend/login.js
@@ -25,7 +25,7 @@ loginForm.addEventListener('submit', async (e) => {
     
     try {
         // Replace with your backend API endpoint
-        const response = await fetch('https://https://school-dress-inventory-production.up.railway.app//api/auth/login', {
+        const response = await fetch('https://school-dress-inventory-production.up.railway.app/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
