33bd20d5c6b61f82b7e2420ea914a6fc75b5cadb
ParthUppal523 <uppalparth523@gmail.com>
2025-11-01 07:11:46 +0530

    Update db.js

--- school-inventory-backend/db.js
+++ school-inventory-backend/db.js
@@ -1,20 +1,24 @@
 // db.js
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
-});
+let db;
 
-db.connect(err => {
-  if (err) console.error('❌ Database connection failed:', err);
-  else console.log('✅ Database connected successfully');
-});
+try {
+  db = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
+  });
+
+  console.log('✅ Database connected successfully');
+} catch (err) {
+  console.error('❌ Database connection failed:', err.message);
+}
+
 
 export default db;
